e7dabc61e6ff08609495c520699c552a99695f0c
Omkar <omkar1jadhav0@gmail.com>
2025-09-28 17:08:40 +0530

    minor changes

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ud636&*c1v^e!u+0zw&hc#kj1w(%ibi%4-bz1h=(ooif-g#gh3'
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
